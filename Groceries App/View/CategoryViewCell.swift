63a0a29129ce431a5079975658ae05f25d5c407a
Fazlı Altun <32793348+fazlialtunn@users.noreply.github.com>
2024-02-21 00:36:57 +0300

    Last version of this app. -Beatiful home page with basic collection view

---
+++ Groceries App/View/CategoryViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryViewCell.swift
+//  Groceries App
+//
+//  Created by Fazli Altun on 20.02.2024.
+//
+
+import UIKit
+
+class CategoryViewCell: UICollectionViewCell {
+    
+}
---
+++ Groceries App/ViewModel/CategoriesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CategoriesViewModel.swift
+//  Groceries App
+//
+//  Created by Fazli Altun on 21.02.2024.
+//
+
+import Foundation
